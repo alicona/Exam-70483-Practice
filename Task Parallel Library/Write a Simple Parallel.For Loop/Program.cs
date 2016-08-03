99bdb962b431aec596ed544d18f220ed3fd59b1d
Alain Licona <alicona@structural.net>
2016-08-03 09:09:24 -0400

    Simple Pararrer.For Loop

---
+++ Task Parallel Library/Write a Simple Parallel.For Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Write_a_Simple_Parallel.For_Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            long totalSize = 0;
+
+            String[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 1) {
+                Console.WriteLine("There are no Arguments");
+                return;
+            }
+            if (!Directory.Exists(args[1]))
+            {
+                Console.WriteLine("The directory doesnt Exists");
+                return;
+            }
+
+            String[] files = Directory.GetFiles(args[1]);
+            Parallel.For(0, files.Length,
+                            index => { FileInfo fi = new FileInfo(files[index]);
+                                long size = fi.Length;
+                                Interlocked.Add(ref totalSize, size);
+                            });
+            Console.WriteLine("Directory '{0}': ", args[1]);
+            Console.WriteLine("{0:N0} files, {1:N0} bytes", files.Length, totalSize);
+            Console.ReadKey();
+
+        }
+    }
+}
