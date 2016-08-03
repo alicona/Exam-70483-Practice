e56c2a6d58f76eb9845a88e0ebf7c369f49653f1
Alain Licona <alicona@structural.net>
2016-08-03 09:44:56 -0400

    Parallel ForEach with Thread-Local variables

---
+++ Task Parallel Library/Pararllel.ForEach with Thread-Local variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pararllel.ForEach_with_Thread_Local_variables
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = Enumerable.Range(0, 1000000).ToArray();
+            long total = 0;
+
+            Parallel.ForEach<int, long>(nums,
+                                                            ()=>0,
+                                                            (j, loop,subtotal)=>
+                                                            {
+                                                                subtotal += j;
+                                                                return subtotal;
+                                                            },
+                                                            (finalResult)=>Interlocked.Add(ref total, finalResult)
+                                        );
+            Console.WriteLine("The total from Parallel.ForEach is {0:N0}", total);
+            Console.ReadKey();
+        }
+    }
+}
