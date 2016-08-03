6e362fb389a4c59c05706b7e5fd6906a0d9c3d07
Alain Licona <alicona@structural.net>
2016-08-03 10:00:26 -0400

    Cancel Parallel Loops

---
+++ Task Parallel Library/Handle Exceptions in Parallel Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handle_Exceptions_in_Parallel_Loops
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
