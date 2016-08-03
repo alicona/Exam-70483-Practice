bd6f660c4cf4f08fac263f47a82dd4168f97260d
Alain Licona <alicona@structural.net>
2016-08-03 10:38:59 -0400

    Handle exceptions in Parallel Loops

--- Task Parallel Library/Handle Exceptions in Parallel Loops/Program.cs
+++ Task Parallel Library/Handle Exceptions in Parallel Loops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,51 @@ namespace Handle_Exceptions_in_Parallel_Loops
     {
         static void Main()
         {
+            byte[] data = new byte[5000];
+            Random r = new Random();
 
+            r.NextBytes(data);
+
+            try
+            {
+                ProcessDataInParallel(data);
+            }
+            catch (AggregateException ae)
+            {
+
+                foreach (var ex in ae.InnerExceptions)
+                {
+                    if (ex is ArgumentException)
+                        Console.WriteLine(ex.Message);
+                    else
+                        throw ex;
+                }
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        private static void ProcessDataInParallel(byte[] data) {
+            var exceptions = new ConcurrentQueue<Exception>();
+
+            Parallel.ForEach(data, d=>
+            {
+                try
+                {
+                    if (d < 0x3)
+                        throw new ArgumentException(String.Format("Value is {0:x}. Elements must be creater than 0x03", d));
+                    else
+                        Console.Write(d + " ");
+                }
+                catch (Exception e )
+                {
+
+                    exceptions.Enqueue(e);
+                }
+            });
+
+            if (exceptions.Count > 0) throw new AggregateException(exceptions);
         }
     }
 }
