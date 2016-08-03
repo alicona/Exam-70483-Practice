1a49905976bc6f411d63c596718a58a9a57e17cb
Alain Licona <alicona@structural.net>
2016-08-03 09:34:39 -0400

    Parallel.For with Thread-Local

---
+++ Task Parallel Library/Parallel.For Loop with Thread-Local Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parallel.For_Loop_with_Thread_Local_Variables
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] nums = Enumerable.Range(0, 1000000).ToArray();
+            long total = 0;
+
+            System.Threading.Tasks.Parallel.For<long>(0, nums.Length, () => 0, (j, loop, subtotal) =>
+               {
+                   subtotal += nums[j];
+                   return subtotal;
+               },
+                (x) => Interlocked.Add(ref total, x)
+            );
+
+            Console.WriteLine("The total is {0:N0}", total);
+            Console.WriteLine("Press any key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
