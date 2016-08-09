49dc63a2484a7b055c8de3d6598d080c7fe4db5d
Alain Licona <conexionadsl@gmail.com>
2016-08-09 16:11:58 -0400

    Implement IComparable to Sort and Array of objects

---
+++ ArraysAndTuples/ArraysAndTuples/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndTuples
+{
+    class Person : IComparable<Person>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int CompareTo(Person other) {
+            if (other == null) return 1;
+
+            int result = string.Compare(this.LastName, other.LastName);
+            if (result== 0)
+            {
+                result = string.Compare(this.FirstName, other.FirstName);
+            }
+            return result;
+        }
+    }
+}
---
+++ ArraysAndTuples/ArraysAndTuples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = {
+                new Person { FirstName="Damon", LastName="Hill" },
+                new Person { FirstName="Niki", LastName="Lauda" },
+                new Person { FirstName="Ayrton", LastName="Senna" },
+                new Person { FirstName="Graham", LastName="Hill" }
+            };
+
+            Array.Sort(persons);
+            foreach (var p in persons)
+            {
+                Console.WriteLine(String.Format("{0} {1}", p.FirstName, p.LastName));
+            }
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
