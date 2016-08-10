dfcb969e504594ea99e2d2ebdc1cc8463180f77c
Alain Licona <conexionadsl@gmail.com>
2016-08-10 13:53:57 -0400

    User Defined Casts

---
+++ Operators/UserDefinedCasts/Currency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+
+namespace UserDefinedCasts
+{
+    public struct Currency
+    {
+        public uint Dollars;
+        public ushort Cents;
+
+        public Currency(uint dollars, ushort cents)
+        {
+            this.Dollars = dollars;
+            this.Cents = cents;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("${0}.{1,-2:00}", Dollars, Cents);
+        }
+
+        public static implicit operator float(Currency val)
+        {
+            return val.Dollars + (val.Cents/100.0f);
+        }
+
+        public static explicit operator Currency(float value)
+        {
+            checked
+            {
+                uint dollars = (uint)value;
+                ushort cents = Convert.ToUInt16((value - dollars) * 100);
+                return new Currency(dollars, cents);
+            }
+        }
+    }
+}
---
+++ Operators/UserDefinedCasts/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedCasts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserDefinedCasts();
+            Console.ReadKey();
+        }
+
+        static void  UserDefinedCasts()
+        {
+            try
+            {
+                Currency balance = new Currency(50,35);
+
+                Console.WriteLine(balance);
+                Console.WriteLine("balance is " + balance);
+                Console.WriteLine("balance is (using ToString()) " + balance.ToString());
+
+                float balance2 = balance;
+
+                Console.WriteLine("After converting to float, = " + balance2);
+
+                balance = (Currency) balance2;
+
+                Console.WriteLine("After converting back to Currency, = " + balance);
+                Console.WriteLine("Now attempt to convert out of range value of " +
+                                  "-$50.50 to a Currency:");
+
+                checked
+                {
+                    balance = (Currency) (-50.50);
+                    Console.WriteLine("Result is " + balance.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception occurred: " + e.Message);
+            }
+        }
+    }
+}
