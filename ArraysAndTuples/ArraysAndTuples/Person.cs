553632e06d8ca7c642c518faf4e8251328d717eb
Alain Licona <conexionadsl@gmail.com>
2016-08-10 07:17:52 -0400

    Using IComparer

--- ArraysAndTuples/ArraysAndTuples/Person.cs
+++ ArraysAndTuples/ArraysAndTuples/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ArraysAndTuples
 {
-    class Person : IComparable<Person>
+    public class Person : IComparable<Person>
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -20,6 +20,39 @@ namespace ArraysAndTuples
                 result = string.Compare(this.FirstName, other.FirstName);
             }
             return result;
+        }
+    }
+
+    public enum PersonCompareType
+    {
+        FirstName,
+        LastName
+    }
+
+    public class PersonComparer : IComparer<Person>
+    {
+        private PersonCompareType compareType;
+
+        public PersonComparer(PersonCompareType compareType)
+        {
+            this.compareType = compareType;
+        }
+
+        public int Compare(Person x, Person y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return 1;
+            if (y == null) return -1;
+
+            switch (compareType)
+            {
+                case PersonCompareType.FirstName:
+                    return string.Compare(x.FirstName, y.FirstName);
+                case PersonCompareType.LastName:
+                    return string.Compare(x.LastName, y.LastName);
+                default:
+                    throw new ArgumentException("unexpected compare type");
+            }
         }
     }
 }
--- ArraysAndTuples/ArraysAndTuples/Program.cs
+++ ArraysAndTuples/ArraysAndTuples/Program.cs
@@ -8,24 +8,40 @@ namespace ArraysAndTuples
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Person[] persons = {
+        static Person[] persons = {
                 new Person { FirstName="Damon", LastName="Hill" },
                 new Person { FirstName="Niki", LastName="Lauda" },
                 new Person { FirstName="Ayrton", LastName="Senna" },
                 new Person { FirstName="Graham", LastName="Hill" }
             };
 
+        static void Main(string[] args)
+        {
+            
+            UsingIComparable();
+            UsingIComparer();
+            Console.ReadKey();
+
+        }
+
+        static void UsingIComparable() {
+            Console.WriteLine("IComparable\n");
             Array.Sort(persons);
             foreach (var p in persons)
             {
                 Console.WriteLine(String.Format("{0} {1}", p.FirstName, p.LastName));
             }
-            Console.ReadKey();
-
         }
 
+        static void UsingIComparer() {
+            Console.WriteLine("IComparer\n");
+            Array.Sort(persons, new PersonComparer(PersonCompareType.FirstName));
+
+            foreach (var p in persons)
+            {
+                Console.WriteLine(String.Format("{0} {1}", p.FirstName, p.LastName));
+            }
+        }
         
     }
 }
